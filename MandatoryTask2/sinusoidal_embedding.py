97b551cbd62025aed941f517a088e7698b39d20a
Paddington <paddington.zhu@gmail.com>
2026-05-20 23:40:41 +0200

    create repo

---
+++ MandatoryTask2/sinusoidal_embedding.py
@@ -0,0 +1,22 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+
+class SinusoidalEmbedding(nn.Module):
+    def __init__(self, size: int, scale: float = 1.0):
+        super().__init__()
+        self.size = size
+        self.scale = scale
+
+    def forward(self, x: torch.Tensor):
+        x = x * self.scale
+        half_size = self.size // 2
+        emb = torch.log(torch.Tensor([10000.0])) / (half_size - 1)
+        emb = torch.exp(-emb * torch.arange(half_size))
+        emb = x.unsqueeze(-1) * emb.unsqueeze(0)
+        emb = torch.cat((torch.sin(emb), torch.cos(emb)), dim=-1)
+        return emb
+
+    def __len__(self):
+        return self.size
